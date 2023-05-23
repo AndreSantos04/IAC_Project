a5e84d1afc91954e7b5a8b4fd23158ae2b99fe93
TUYA <29687422+tuya4real@users.noreply.github.com>
2023-05-23 13:36:25 +0100

    Teclado e conversor
    Adicionei um teclado que mostra no display qual a tecla premida, já convertida de Hexadecimal para decimal

--- grupo55.asm
+++ grupo55.asm
@@ -2,3 +2,160 @@
 ;   107041- André Antunes Santos
 ;   107052 - Tomás Maria Agostinho Bernardino
 ;   88571 - Artur Miguel e Gaio Lopes dos Santos Pinto
+
+
+
+; *********************************************************************************
+; * Constantes
+; *********************************************************************************
+DISPLAYS		    EQU  0A000H	; endereço do periférico que liga aos displays
+TEC_LIN				EQU 0C000H	; endereço das linhas do teclado (periférico POUT-2)
+TEC_COL				EQU 0E000H	; endereço das colunas do teclado (periférico PIN)
+LINHA_TECLADO	    EQU  0010H	; linha a testar 1 bit a esquerda da linha maxima (8b)
+MASCARA				EQU 0FH		; para isolar os 4 bits de menor peso, ao ler as colunas do teclado
+TECLA_ESQUERDA			EQU 1		; tecla na primeira coluna do teclado (tecla C)
+TECLA_DIREITA			EQU 2		; tecla na segunda coluna do teclado (tecla D)
+
+COMANDOS				EQU	6000H			; endereço de base dos comandos do MediaCenter
+
+DEFINE_LINHA    		EQU COMANDOS + 0AH		; endereço do comando para definir a linha
+DEFINE_COLUNA   		EQU COMANDOS + 0CH		; endereço do comando para definir a coluna
+DEFINE_PIXEL    		EQU COMANDOS + 12H		; endereço do comando para escrever um pixel
+APAGA_AVISO     		EQU COMANDOS + 40H		; endereço do comando para apagar o aviso de nenhum cenário selecionado
+APAGA_ECRÃ	 		EQU COMANDOS + 02H		; endereço do comando para apagar todos os pixels já desenhados
+SELECIONA_CENARIO_FUNDO  EQU COMANDOS + 42H		; endereço do comando para selecionar uma imagem de fundo
+TOCA_SOM				EQU COMANDOS + 5AH		; endereço do comando para tocar um som
+
+
+LINHA        		EQU  16        ; linha do boneco (a meio do ecrã)
+COLUNA			EQU  30        ; coluna do boneco (a meio do ecrã)
+
+MIN_COLUNA		EQU  0		; número da coluna mais à esquerda que o objeto pode ocupar
+MAX_COLUNA		EQU  63        ; número da coluna mais à direita que o objeto pode ocupar
+ATRASO			EQU	400H		; atraso para limitar a velocidade de movimento do boneco
+
+LARGURA			EQU	5		; largura do boneco
+COR_PIXEL			EQU	0FF00H	; cor do pixel: vermelho em ARGB (opaco e vermelho no máximo, verde e azul a 0)
+
+; *********************************************************************************
+; * Dados 
+; *********************************************************************************
+	PLACE       1000H
+pilha:
+	STACK 100H			; espaço reservado para a pilha 
+						; (200H bytes, pois são 100H words)
+SP_inicial:				; este é o endereço (1200H) com que o SP deve ser 
+						; inicializado. O 1.º end. de retorno será 
+						; armazenado em 11FEH (1200H-2)
+							
+DEF_BONECO:					; tabela que define o boneco (cor, largura, pixels)
+	WORD		LARGURA
+	WORD		COR_PIXEL, 0, COR_PIXEL, 0, COR_PIXEL		; # # #   as cores podem ser diferentes de pixel para pixel
+     
+
+; *********************************************************************************
+; * Código
+; *********************************************************************************
+PLACE   0                     ; o código tem de começar em 0000H
+inicio:
+	MOV  SP, SP_inicial		; inicializa SP para a palavra a seguir
+						; à última da pilha
+                            
+     MOV  [APAGA_AVISO], R1	; apaga o aviso de nenhum cenário selecionado (o valor de R1 não é relevante)
+     MOV  [APAGA_ECRÃ], R1	; apaga todos os pixels já desenhados (o valor de R1 não é relevante)
+	 MOV	R1, 0			; cenário de fundo número 0
+     MOV  [SELECIONA_CENARIO_FUNDO], R1	; seleciona o cenário de fundo
+     
+repete:   
+	CALL	teclado			; leitura às teclas
+
+
+    CALL converte_numero   ; retorna R9 com a tecla premida
+	
+	MOV R5, DISPLAYS
+    MOV [R5], R9 
+
+	JMP repete
+
+
+
+; **********************************************************************
+; TECLADO - Faz uma leitura às teclas de uma linha do teclado e retorna o valor lido
+;
+;
+; Retorna: 	R0 - valor lido das colunas do teclado (0, 1, 2, 4, ou 8)
+;			R1 - Valor das linhas	
+; **********************************************************************
+teclado:
+	PUSH	R2
+	PUSH	R3
+	PUSH	R5
+
+loop_linha:
+    MOV R1, LINHA_TECLADO   ; por linha a 0001 0000 - para testar qual das linhas foi clicada
+
+espera_tecla:
+    SHR R1, 1           ; dividir por 2 para testar as varias linhas do teclado
+	JZ  loop_linha      ; se for zero recomeçar o ciclo, caso contrario testar colunas 
+
+
+	MOV  R2, TEC_LIN   ; endereço do periférico das linhas
+	MOV  R3, TEC_COL   ; endereço do periférico das colunas
+	MOV  R5, MASCARA   ; para isolar os 4 bits de menor peso, ao ler as colunas do teclado
+	MOVB [R2], R1      ; escrever no periférico de saída (linhas)
+	MOVB R0, [R3]      ; ler do periférico de entrada (colunas)
+	AND  R0, R5        ; elimina bits para além dos bits 0-3
+	CMP	R0, 0
+	JZ espera_tecla		; se nenhuma tecla premida, repete
+
+
+	POP	R5
+	POP	R3
+	POP	R2
+	RET
+
+
+; **********************************************************************
+; Rotina
+;
+; Converte_numero - converte o numero da linha e da coluna da tecla premida
+;		            no numero/letra premida
+;
+;PARAMETROS: R1 - numero da linha premida
+;            R0 - numero da coluna
+;
+;RETORNA: R9 - tecla clicada
+; **********************************************************************
+
+converte_numero:
+    PUSH R0
+    PUSH R1
+    PUSH R2
+    MOV R9, 0   ; inicializar contador da linha a zero
+    MOV R10, 0  ; inicializar contador da coluna a zero
+    MOV R2, 4   ; Sera no final usado para multiplicar por 4
+
+ciclo_converte_linha:
+    SHR R1, 1       ;vou dividindo por 2 para ver quantos ciclos se completa até chegar a zero
+    CMP R1, 0       ; quando chega a zero, converti de numero binario da linha ( 1 a 8)
+                    ; para numero entre 0 a 3
+    JZ  ciclo_converte_coluna   ; que vou usar na expressao no final para me dar tecla clicada
+    ADD R9, 1          ; vou adicionando 1 ao contador
+    JMP ciclo_converte_linha    ; volto ao inicio
+
+ciclo_converte_coluna:
+    SHR R0, 1       ; depois de ter numero das linhas faço o mesmo para as colunas
+    CMP R0, 0       ; converto de 1 a 8 binario para 0 a 3 decimal
+    JZ final_converte
+    ADD R10, 1
+    JMP ciclo_converte_coluna
+
+final_converte:
+
+    MUL R9, R2      ; Usando a expressao: 
+                    ;Tecla = 4 x Num_decimal_linha + num_decimal_col
+    ADD R9, R10     ; retorno o R9 
+    POP R2
+    POP R1
+    POP R0
+    RET
